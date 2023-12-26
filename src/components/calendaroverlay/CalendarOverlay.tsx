04c8f5fcb5ee2ac3b7b8c2484547be123a18cfc7
Ruben Hegedus <hegeruben@gmail.com>
2023-12-26 13:13:31 +0100

    Moved getMonthName to Utils.ts

--- src/components/calendaroverlay/CalendarOverlay.tsx
+++ src/components/calendaroverlay/CalendarOverlay.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Overlay from "../Overlay";
 
 import "./CalendarOverlay.css";
+import { getMonthName } from "../../utils/Utils";
 
 export function CalendarOverlay(props: {
   shown: boolean;
@@ -163,34 +164,4 @@ function createWeek(year: number, month: number, setSelectedDay: React.Dispatch<
   }
 
   return out;
-}
-
-function getMonthName(month: number) {
-  switch (month) {
-    case 0:
-      return "January";
-    case 1:
-      return "February";
-    case 2:
-      return "March";
-    case 3:
-      return "April";
-    case 4:
-      return "May";
-    case 5:
-      return "June";
-    case 6:
-      return "July";
-    case 7:
-      return "August";
-    case 8:
-      return "September";
-    case 9:
-      return "October";
-    case 10:
-      return "November";
-    case 11:
-      return "December";
-  }
-  return "Invalid";
 }
